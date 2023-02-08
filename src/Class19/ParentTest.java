1e36a4bcfda4e1545678723410643f8373295954
IsabelleFerreira95 <isabelle.winchester93@gmail.com>
2023-02-07 19:06:07 -0500

    Class 19 saved

---
+++ src/Class19/ParentTest.java
@@ -0,0 +1,7 @@
+package Class19;
+
+public class ParentTest {
+
+        // new Parent() Cannot create an object of the parent class since constructor is private
+    }
+
---
+++ src/Class19/TestingConstructors.java
@@ -0,0 +1,7 @@
+package Class19;
+
+public class TestingConstructors {
+    public static void main(String[] args) {
+        SubClass sub=new SubClass();
+    }
+}
